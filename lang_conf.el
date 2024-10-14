35dae19fa43bce28c29ba0b1bc699a53b99099dc
Joseph Palomino <saiborack@gmail.com>
2024-10-14 17:29:03 -0500

    Add extra config

---
+++ lang_conf.el
@@ -0,0 +1,19 @@
+;;; Parent symbol highlight
+(show-paren-mode)
+
+;;; Compress function to hide and show
+(global-set-key (kbd "M-b h h") 'hs-hide-block)
+(global-set-key (kbd "M-b h s") 'hs-show-block)
+(global-set-key (kbd "M-b h t") 'hs-toggle-hiding)
+(global-set-key (kbd "M-b h a") 'hs-hide-all)
+(global-set-key (kbd "M-b h s") 'hs-hide-level)
+
+;;; Complete ( ' " ...
+(use-package autopair
+  :straight t
+  :ensure t
+  :config
+  (autopair-global-mode))
+
+
+(provide 'lang_conf)
